23556ae632206a81c8a39deb1a62204b65ca1ae3
Elia Rezai <5152@htl.rennweg.at>
2022-11-17 23:04:08 +0100

    v1

---
+++ Frontend/Grafische-Darstellung/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="nav">
+       
+        <a href="#">Startseite</a>
+        <a href="#">Praxis</a>
+        <a href="#">Therapien</a>
+        <a href="#">Öffnungzeiten</a>
+        <a style="color: skyblue;" href="#">Registrierung</a>
+        <img src="@/assets/g2.png" width="100" height="100"/>
+    </div>
+
+</template>
+<script>
+
+export default {
+   name:'HeadeR',
+}
+</script>
+<style>
+.nav{
+    
+    overflow:hidden;
+    display: flex;
+    margin-left: 10vw;
+    
+
+}
+
+.nav a {
+    color: black;
+    padding: 14px 16px;
+    text-align: top;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: -10px;
+    overflow: hidden;
+    margin-top:10px;
+    margin-bottom:10vh;
+    font-size: larger;
+}
+
+.nav a:hover{
+    background: white;
+    color: #0088ff;
+}
+
+</style>
---
+++ Frontend/Grafische-Darstellung/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h4>halloooo</h4>
+    </div>
+    
+  </template>
+  
+  <script>
+    export default {
+        name: "home",
+    }
+  </script>
+  
+  <style>
+ 
+  </style>
+  
