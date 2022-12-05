27648890636543523b9ca9cf3a27f39d30717156
Elia Rezai <5152@htl.rennweg.at>
2022-12-05 13:41:57 +0100

    added startseite

---
+++ Frontend/src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h4>halloooo</h4>
+    </div>
+    
+  </template>
+  
+  <script>
+export default {
+  name: 'HelloWorld', 
+  name: 'home',
+  components:{
+  },
+
+  
+}
+</script>
+  
+  <style>
+ 
+  </style>
+  
