34145d27042a2c955d6dc96af5c8c22e7b8a15d8
Lukas Wezowicz <WEZ22849@spengergasse.at>
2023-06-20 12:15:42 +0200

    opening

--- Frontend/src/views/OpeningView.vue
+++ Frontend/src/views/OpeningView.vue
@@ -1,15 +1,7 @@
 <template>
     <div class="openingView">
         
-        <ul>
-            <li>Im Backend das Modell erweitern, sodass Öffnungszeiten pro Wochentag gespeichert werden können.</li>
-            <li>Im Backend das Modell erweitern, sodass einzelne Tage mit besonderen Öffnungszeiten gespeichert werden können.</li>
-            <li>Daten im Backend seeden.</li>
-            <li>Die Daten für das aktuelle Monat hier anzeigen.</li>
-            <li>Für die Vergabe von besonderen Öffnungszeiten kann vielleicht die Kalenderansicht
-                wie bei Termine verwalten verwendet werden, um ordinationsfreie Tage einfacher
-                eingeben zu können.</li>
-        </ul>
+        
        
        
         <div class="opening-hours">
