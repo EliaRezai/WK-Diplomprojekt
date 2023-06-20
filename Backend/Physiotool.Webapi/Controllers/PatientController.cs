175594c1b251ba6ec3a4d25ddff711f7b334110e
Dean-Christoph Neuhold <NEU22840@spengergasse.at>
2023-06-20 14:12:21 +0200

    Add info site.

--- Backend/Physiotool.Webapi/Controllers/PatientController.cs
+++ Backend/Physiotool.Webapi/Controllers/PatientController.cs
@@ -1,7 +1,9 @@
 using Bogus;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Physiotool.Application.Infrastructure;
+using Physiotool.Application.Model;
 using System;
 using System.Linq;
 
@@ -19,6 +21,31 @@ namespace Physiotool.Webapi.Controllers
         public IActionResult GetAllPatients()
         {
             return Ok(_db.Patients.ToList());
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetPatient(Guid guid)
+        {
+            var patient = _db.Patients
+                .Where(p => p.Guid == guid)
+                .Select(p => new
+                {
+                    p.Firstname,
+                    p.Lastname,
+                    p.Email,
+                    Appointments = p.Appointments
+                        .Where(a => !(a.AppointmentState is DeletedAppointmentState))
+                        .Select(a => new
+                        {
+                            a.Guid,
+                            Confirmed = a.AppointmentState is ConfirmedAppointmentState,
+                            a.Date,
+                            a.Time
+                        }).ToList()
+                })
+                .FirstOrDefault();
+            if (patient is null) { return NotFound(); }
+            return Ok(patient);
         }
     }
 }
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import store from '../store.js'
 import HomeView from '../views/HomeView.vue'
+import PatientView from '../views/PatientView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
@@ -30,6 +31,10 @@ const router = createRouter({
       name: 'termin',
       component: () => import('../views/TerminView.vue'),
     },
+    {
+      path: '/patient/:id',
+      component: PatientView,
+    },    
     {
       path: '/terminverwaltung',
       name: 'terminverwaltung',
